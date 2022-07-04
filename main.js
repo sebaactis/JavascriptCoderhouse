72544da636c22e2ae8dc43e1a83b96975cdf9c0e
sebastian.actis <sebaactis@gmail.com>
2022-07-04 20:33:56 -0300

    correccion variables

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let IngresarNumero = parseInt(prompt("Ingrese un numero para ver la tabla del 1 al 10 de dicho numero"));
+let ingresarNumero = parseInt(prompt("Ingrese un numero para ver la tabla del 1 al 10 de dicho numero"));
 
-let mensajeTabla = "La tabla del " + IngresarNumero + " " + "es:"
+let mensajeTabla = "La tabla del " + ingresarNumero + " " + "es:"
     alert(mensajeTabla);
 
 
 for (let i = 1; i <= 10; i++) {
-       let tablaNumero = IngresarNumero + " " + "x " + i + " " + "es: " +  (IngresarNumero * i);
-       alert(tablaNumero);    
+       let tablaNumero = ingresarNumero + " " + "x " + i + " " + "es: " +  (ingresarNumero * i);
+       alert(tablaNumero);  
 }
