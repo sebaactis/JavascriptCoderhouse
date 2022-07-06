8e7bdfc4fe4124faa5aa9dbc8cfa1044c32391ab
Seba Actis <sebaactis@gmail.com>
2022-07-06 14:44:22 -0300

    correccion textos alerts

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ function valorPesos(pesos) {
     return pesos + pesos * 0.3 + pesos * 0.35;
 }
 
-alert('Bienvenido, en este simulador vas a poder ver como afecta los impuestos a las comprar en moneda extranjera');
+alert('Bienvenido, en este simulador vas a poder ver como afectan los impuestos a las compras en moneda extranjera');
 alert('Todos los valores son representados en ARS, por favor, tengalo en cuenta a la hora de ingresar datos');
 alert('El impuesto pais es un 30% del valor original');
 alert('El impuesto ganancias es un 35% del valor original');
-alert('Para empezar, debes ingresar 5 numeros mayores a 0, y veras como aumenta el valor por los impuestos');
+alert('Para empezar, debes ingresar 5 numeros mayores a 0, y veras como aumenta el valor a raiz de los impuestos agregados');
 
 
 for (let i = 1; i < 5; i++) {
